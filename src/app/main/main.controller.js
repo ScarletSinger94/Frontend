2fc2b7c808413bc8982cf78542b0b39586ad72f8
ScarletSinger94 <firstoffour94@aol.com>
2017-04-28 16:49:12 -0400

    Yes

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,8 +2,22 @@ export class MainController {
   constructor ($http) {
     'ngInject';
     this.$http=$http;
+    this.getMessages();
+  }
+  getMessages(){
+    console.log("Get");
+    var vm = this;
+    this.$http.get('http://localhost:5000/api/message').then(function(result){
+      console.log(result);
+      vm.messages = result.data;
+    });
+
+  }
+   deleteMessages(){
+  console.log("delete");
   }
   postMessage(){
+    console.log("Post");
     this.$http.post('http://localhost:5000/api/message', {msg: this.message});
   }
 }
